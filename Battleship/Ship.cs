43d236aeef7c7f0acfbcac8b210e2a4f6eb78a91
Russell Payne <ryois@ryois.me>
2023-09-01 17:07:19 -0400

    moving computers lol

---
+++ Battleship/Ship.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    internal class Ship
+    {
+        private Coord2D position;
+        private byte Length;
+        private DirectionType Direction;
+        private Coord2D[] Points;
+        private List<Coord2D> DamagedPoints;
+
+
+        public Ship(Coord2D position, byte length, DirectionType direction)
+        {
+            this.position = position;
+            Length = length;
+            Direction = direction;
+        }
+
+        public bool CheckIfHit(Coord2D point)
+        {
+            // TO DO
+        }
+
+        public void TakeDamage(Coord2D point)
+        { 
+            // TO DO
+        }
+
+        public void TakeDamage(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            // TO DO
+        }
+
+        public byte GetMaxHealth()
+        {
+            // TO DO
+        }
+
+        public byte GetCurrentHealth()
+        {
+            // TO DO
+        }
+
+        public bool IsDead()
+        {
+            // TO DO
+        }
+
+        public bool CheckIfHit(Coord2D point)
+        {
+            // TO DO 
+        }
+
+    }
+}
